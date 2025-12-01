f8d8f0e108ff4bcf047ac5f0c8a0fa8dc376f00f
maxine duran <johnheaven48@gmail.com>
2025-12-01 13:18:06 +0800

    Launches-SpaceX

---
+++ app/pages/Launches.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-contaier>
+		<!-- title SpaceX Launches -->
+		<h2>
+			<v-icon icon="mdi-rocket" />
+			SpaceX Launches
+		</h2>
+
+		<!-- Handle Loading and Error -->
+		<div v-if="loading">Loading launches</div>
+		<div v-if="error">Error</div>
+
+		<!-- Launches table -->
+		<v-table v-if="launches.length">
+			<thead>
+				<tr>
+					<th>Misson Name</th>
+					<th>Launch Date</th>
+					<th>Launch Site</th>
+					<th>Rocke Name</th>
+					<th>Details</th>
+				</tr>
+			</thead>
+		</v-table>
+	</v-contaier>
+</template>
