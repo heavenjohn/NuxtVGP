eb42e0286c4771a8d717e5ed19a218b9985560a8
maxine duran <johnheaven48@gmail.com>
2025-12-02 21:36:13 +0800

    dynamicaly dispaly

---
+++ app/pages/Rockets/[id].vue
@@ -0,0 +1,19 @@
+<template>
+	<v-container>
+		<div v-if="loading">Loading rocket details...</div>
+		<div v-if="error">Error fetching data.</div>
+
+		<div v-if="rocket">
+			<h2>{{ rocket.name }}</h2>
+			<p>{{ rocket.description }}</p>
+		</div>
+	</v-container>
+</template>
+
+<script lang="ts" setup>
+import { useLazyQuery } from '@vue/apollo-composable'
+import { gql } from 'graphql-tag'
+import { computed, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+
+</script>
